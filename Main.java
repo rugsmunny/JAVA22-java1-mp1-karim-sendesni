1eb0b57ded547ebc6ce2cf034e4bb8f7df3a552e
rugsmunny <49041363+rugsmunny@users.noreply.github.com>
2022-10-22 05:24:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,52 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+class InitField{
+    static int gissning, gissningar = 0, slumpNummer;
+
+    static boolean run = true;
+
+    static Scanner in = new Scanner(System.in);
+}
+public class Main extends InitField{
     public static void main(String[] args) {
 
-        int gissning, gissningar = 0, slumpNummer;
-
-        boolean run = true;
-
-        Scanner in = new Scanner(System.in);
-
-
-        while(run){
-            slumpNummer = new Random().nextInt(101);
-            System.out.print("Gissa på ett nummer från 0 till och med 100: ");
-            do{
-                gissning = in.nextInt();
-                gissningar++;
-                in.nextLine();
-                if (gissning > slumpNummer)
-                    System.out.println("För högt");
-                if(gissning < slumpNummer)
-                    System.out.println("För lågt");
-            }while(gissning != slumpNummer);
-            System.out.println("Grattis! du gissade rätt! Antal gissningar: "+gissningar+"\n");
-            System.out.print("Fortsätta = j / n: ");
-            ;
-            if(in.nextLine().equalsIgnoreCase("n"))
-                run = false;
+        while(run) {
+            runGame();
         }
+    }
 
+    static void runGame(){
+        slumpNummer = new Random().nextInt(101);
+        System.out.print("Gissa på ett nummer från 0 till och med 100: ");
+        do{
+        playerInput();
+        }while(evaluatePlayerGuess( gissning, slumpNummer) != slumpNummer);
+        System.out.println("Grattis! du gissade rätt! Antal gissningar: " + gissningar + "\n");
+        continueQuestion();
+
+    }
+    static void playerInput(){
+        gissning = in.nextInt();
+        gissningar++;
+        in.nextLine();
+    }
+    static int evaluatePlayerGuess(int gissning, int slumpNummer){
+        if ((gissning > slumpNummer)) {
+            System.out.println("För högt");
+
+        } else if (gissning < slumpNummer) {
+                System.out.println("För lågt");
+
+        }return gissning;
+
+    }
+    static void continueQuestion(){
+        System.out.print("Fortsätta = j / n: ");
+        if(in.nextLine().equalsIgnoreCase("n"))
+            run = false;
+    }
+}
 
 
 
